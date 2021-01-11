f2d6e6d87a6a77eb90ee53a16a62e09c820e6c17
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-01-11 20:30:18 +0100

    Cleanup.

--- src/game/bond.h
+++ src/game/bond.h
@@ -1,49 +1,9 @@
 #ifndef _BOND_H_
 #define _BOND_H_
 
-extern unsigned int D_80036630;
-extern unsigned int D_80036634[];
 extern int SFX_80036458;
 extern int SFX_8003645C;
-extern int D_800367F4;
-extern int D_800367F8;
-extern int D_800367FC;
-extern int D_80036800;
-extern int D_80036804;
-extern int D_80036808;
-extern int D_8003680C;
-extern int D_80036810;
-extern int D_80036814;
-extern int D_80036818;
-extern int D_8003681C;
-extern int D_80036820;
-extern int D_80036824;
-extern int D_80036828;
-extern int D_8003682C;
-extern int D_80036830;
-extern int D_80036834;
-extern int D_80036838;
-extern int D_8003683C[];
-extern int D_80036878;
-extern int D_8003687C;
-extern int D_80036880;
-extern int D_80036884;
-extern int D_80036888;
-extern int D_8003688C;
-extern int D_80036890;
-extern int D_80036894;
-extern int status_bar_text_buffer_index;
-extern int display_statusbar;
 extern int copy_1stfonttable;
 extern int copy_2ndfonttable;
-extern int upper_text_buffer_index;
-extern int display_upper_text_window;
-extern int upper_text_window_timer;
-extern int D_800368B4;
-extern unsigned short D_800368B8[];
-extern int D_80036AB8;
-extern int D_80036ABC;
-extern float D_80036AC0;
-extern float D_80036AC4;
 
 #endif
