52db469bb0f63de44436070f1b245d6eba4d2839
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-01-12 20:32:04 +0100

    Cleanup.

--- src/game/intro_logos.h
+++ src/game/intro_logos.h
@@ -1,8 +1,6 @@
 #ifndef _INTRO_LOGOS_H_
 #define _INTRO_LOGOS_H_
 
-extern int dword_CODE_bss_80069550;
-extern int dword_CODE_bss_80069554;
 extern int matrix_buffer_rarelogo_0;
 extern int matrix_buffer_gunbarrel_0;
 extern int matrix_buffer_rarelogo_1;
@@ -10,16 +8,6 @@ extern int matrix_buffer_rarelogo_2;
 extern int matrix_buffer_gunbarrel_1;
 extern int matrix_buffer_intro_backdrop;
 extern int matrix_buffer_intro_bond;
-extern int x;
-extern int y;
-extern int dword_CODE_bss_8006957C;
-extern int dword_CODE_bss_80069580;
-extern int word_CODE_bss_80069584;
-extern int dword_CODE_bss_80069588;
-extern int dword_CODE_bss_8006958C;
-extern int virtualaddress;
-extern int dword_CODE_bss_80069594;
-
 extern unsigned int D_8002A7D0;
 
 #endif
--- src/game/lvl.h
+++ src/game/lvl.h
@@ -6,17 +6,8 @@ struct ramrom_struct {
   unsigned int unk;
 };
 
-extern int D_800483C4;
 extern int clock_timer;
-extern int global_timer;
 extern float global_timer_delta;
-extern unsigned int *ptr_jfont_DL;
-extern int dword_CODE_bss_8008C264;
-extern int dword_CODE_bss_8008C268;
-extern int dword_CODE_bss_8008C26C;
-extern char ramrom_data_target[0x380];
 extern int record_slot_num;
-extern int *address_demo_loaded;
-extern int dword_CODE_bss_8008C5F8;
 
 #endif
