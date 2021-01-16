83e27c69bf6575c2318f7824748644cfcb6f7bf1
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-01-16 22:52:36 +0100

    Docs.

--- src/cfb/cfb.c
+++ src/cfb/cfb.c
@@ -1,5 +1,5 @@
-
-// 16-bit framebuffers.
+// 16-bit double framebuffers.
 #define FRAMEBUFFER_SIZE 320 * 240 * 2
 unsigned char cfb_16_a[FRAMEBUFFER_SIZE];
 unsigned char cfb_16_b[FRAMEBUFFER_SIZE];
+
