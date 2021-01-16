b82d10b0261b6413033bbdd15c4bd461b7b83227
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-01-16 17:41:57 +0100

    Simplified.

--- src/game/initintromatrices.c
+++ src/game/initintromatrices.c
@@ -1,4 +1,11 @@
-#include "intro_logos.h"
+extern int matrix_buffer_rarelogo_0;
+extern int matrix_buffer_gunbarrel_0;
+extern int matrix_buffer_rarelogo_1;
+extern int matrix_buffer_rarelogo_2;
+extern int matrix_buffer_gunbarrel_1;
+extern int matrix_buffer_intro_backdrop;
+extern int matrix_buffer_intro_bond;
+extern unsigned int D_8002A7D0;
 
 void alloc_intro_matrices(void) {
   D_8002A7D0 = 0;
