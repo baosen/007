dc2fe808b1246c3244e25694fbab664dad2199e0
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-01-16 19:25:30 +0100

    Docs.

--- src/game/playerstats_007770.c
+++ src/game/playerstats_007770.c
@@ -1,13 +1,13 @@
 void sub_GAME_7F007770(void) {
-  int sp3C, sp38, temp_s3 = getPlayerCount(), sp30 = get_cur_playernum(),
-                  phi_s0 = 0;
-  if (0 < temp_s3) {
+  int sp3C, sp38, player_count = getPlayerCount(),
+                  current_player = get_cur_playernum(), i = 0;
+  if (0 < player_count) {
     do {
-      set_cur_player(phi_s0);
+      set_cur_player(i);
       sub_GAME_7F08DB08(&sp3C, &sp38);
       store_favorite_weapon_current_player(sp3C, sp38);
-      phi_s0 += 1;
-    } while (phi_s0 != temp_s3);
+      i += 1;
+    } while (i != player_count);
   }
-  set_cur_player(sp30);
+  set_cur_player(current_player);
 }
