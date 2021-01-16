22de0631ddd7faec0e36ff8408fb01db08f2ad57
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-01-16 17:58:59 +0100

    Cleanup.

--- src/game/initmttex.c
+++ src/game/initmttex.c
@@ -4,7 +4,7 @@ void set_mt_tex_alloc(void) {
   extern int ptr_texture_alloc_start;
 
   dword_CODE_bss_8008D090 = 0;
-  if (check_token(1, "-mt") != 0) {
+  if (check_token(1, "-mt")) {
     bytes = (int)(strtol(check_token(1, "-mt"), 0, 0) << 0xA);
   }
   sub_GAME_7F0CBAF4(&ptr_texture_alloc_start, mempAllocBytesInBank(bytes, 4),
