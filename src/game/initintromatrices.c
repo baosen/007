715869d1f5fa40fd535d65368faa2b92cffdfd8c
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-02-13 21:05:03 +0100

    Refactorings.

--- src/game/initintromatrices.c
+++ src/game/initintromatrices.c
@@ -1,13 +1,12 @@
-extern int matrix_buffer_rarelogo_0;
-extern int matrix_buffer_gunbarrel_0;
-extern int matrix_buffer_rarelogo_1;
-extern int matrix_buffer_rarelogo_2;
-extern int matrix_buffer_gunbarrel_1;
-extern int matrix_buffer_intro_backdrop;
-extern int matrix_buffer_intro_bond;
-extern unsigned int D_8002A7D0;
-
 void alloc_intro_matrices(void) {
+  extern unsigned int D_8002A7D0;
+  extern int matrix_buffer_rarelogo_0;
+  extern int matrix_buffer_gunbarrel_0;
+  extern int matrix_buffer_rarelogo_1;
+  extern int matrix_buffer_rarelogo_2;
+  extern int matrix_buffer_gunbarrel_1;
+  extern int matrix_buffer_intro_backdrop;
+  extern int matrix_buffer_intro_bond;
   D_8002A7D0 = 0;
   matrix_buffer_rarelogo_0 = mempAllocBytesInBank(0x80, 4);
   matrix_buffer_gunbarrel_0 = mempAllocBytesInBank(0x40, 4);
