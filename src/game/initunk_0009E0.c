1c31919a80a73dc0601362ab4fba66c8ab6c6a3e
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-02-14 17:19:55 +0100

    Cleanup.

--- src/game/initunk_0009E0.c
+++ src/game/initunk_0009E0.c
@@ -1,4 +1,6 @@
-#include "objecthandler.h"
+extern int D_80036414;
+extern int D_80036418;
+extern int D_8003641C;
 
 void sub_GAME_7F0009E0(int arg0, int arg1, int arg2) {
   D_80036414 = arg0;
--- src/game/objecthandler.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OBJECTHANDLER_
-#define _OBJECTHANDLER_
-
-extern int D_80036410;
-extern int D_80036414;
-extern int D_80036418;
-extern int D_8003641C;
-
-#endif
