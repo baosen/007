3e8e1e5ded94a66965fb9b6b3eda75b24b12520d
Bao Chi Tran Nguyen <chibaosen@gmail.com>
2021-02-21 13:15:03 +0100

    Refactorings.

--- src/game/random/random.c
+++ src/game/random/random.c
@@ -1,10 +1,10 @@
-unsigned long long random_val = 0xAB8D9F7781280783;
+unsigned long long game_random_seed = 0xAB8D9F7781280783;
 
 asm(R"
 glabel get_rand_val
-  lui   $a0, %hi(random_val)
-  ld    $a0, %lo(random_val)($a0)
-  lui   $at, %hi(random_val)
+  lui   $a0, %hi(game_random_seed)
+  ld    $a0, %lo(game_random_seed)($a0)
+  lui   $at, %hi(game_random_seed)
   dsll32 $a2, $a0, 0x1f
   dsll  $a1, $a0, 0x1f
   dsrl  $a2, $a2, 0x1f
@@ -17,7 +17,7 @@ glabel get_rand_val
   andi  $a0, $a0, 0xfff
   xor   $a0, $a0, $a2
   dsll32 $v0, $a0, 0
-  sd    $a0, %lo(random_val)($at)
+  sd    $a0, %lo(game_random_seed)($at)
   jr    $ra
   dsra32 $v0, $v0, 0
 ");
@@ -25,8 +25,8 @@ glabel get_rand_val
 asm(R"
 glabel set_rand_val
   daddiu $a0, $a0, 1
-  lui   $at, %hi(random_val)
-  sd    $a0, %lo(random_val)($at)
+  lui   $at, %hi(game_random_seed)
+  sd    $a0, %lo(game_random_seed)($at)
   jr    $ra
   li    $a0, 0
 ");
